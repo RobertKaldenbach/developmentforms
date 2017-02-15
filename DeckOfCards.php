a0fcda24be3e06d1a9aa866d075a9014e186699b
jbabinec1 <jbabinec1@gmail.com>
2017-02-15 15:44:23 -0500

    Add files via upload
    A copy of both assignments

---
+++ DeckOfCards.php
@@ -0,0 +1,51 @@
+<?php
+
+/*** This script stimulates a deck of cards with the ability to shuffle them using a single class. ***/ 
+
+ 
+ class deck {
+    
+	 
+    //Builds deck with two arrays to hold different suits and faces.      
+    
+    public static function cards () 
+        
+    $suits = array ( "Spades", "Hearts", "Clubs", "Diamonds" ); 
+    $faces = array ( "Two","Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen",     
+                    "King", "Ace" ); 
+    
+  
+    // Cards array to hold the card values. 
+    
+     $cards = array(); 
+      
+    foreach ($suits as $suit) {
+        foreach ($faces as $face) {
+            $cards[]= array ("face"=>$face, "suit"=>$suit);
+            
+        }
+    }
+    
+    return $cards; 
+} 
+        
+  // This second function shuffles the array of cards. 
+
+public static function shuffle (array $cards) {
+    
+    $total_cards = count($cards);
+    
+    foreach ($cards as $index => $card) {
+        
+         // Picks a random card.
+        $card2_index = mt_rand(1, $total_cards) - 1;
+			 $card2 = $cards[$card2_index];
+    }
+    
+    return $cards; 
+
+}
+}
+    
+
+?>
---
+++ FindTriangle.js
@@ -0,0 +1,33 @@
+
+/*** This JS method finds the correct triangle based off corresponding sides. Throws error message on the screen if it isn't a triangle. ****/
+
+
+var Triangle = function(a,b,c) { this.triangle = [a,b,c].sort() }
+
+// To find the type of triangle 
+Triangle.prototype.kind = function() {
+  
+  //Sides smallest to largest 
+  var a = this.triangle[0];
+	var b = this.triangle[1];
+	var c = this.triangle[2];
+  
+  
+ // Returns the correct triangle 
+  
+	if (a === b && b === c)
+		return "equilateral";
+	if(a === b || b === c)
+		return "isosceles";
+	return "scalene";
+  
+  //Error message in case it is not a triangle 
+ 
+  if (a < 0) 
+		throw "Only positive length sides are allowed";
+	if (a * b * c == 0)
+		throw "Triangle cannot have zero area";
+	if (a + b < c)
+		throw "Not a valid triangle";  
+  
+}; 
